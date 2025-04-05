86a2ba46f3afdc44979593eced198b54fb489a28
Jasmine <jasmineliu331@gmail.com>
2025-04-05 16:30:25 -0700

    basic pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import { Route, Routes } from 'react-router'
-import Home from './pages/Home'
+import { Route, Routes } from 'react-router';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Login from './pages/Login';;
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   )
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import DefaultLayout from "../components/DefaultLayout";
+
 function Home() {
   return (
-    <div className="home">
-      <h1>Welcome to the Home Page!</h1>
-      <p>This is the main page of our application.</p>
-    </div>
+    <DefaultLayout>
+      <div className="home">
+        <h1>Welcome to the Home Page!</h1>
+        <p>This is the main page of our application.</p>
+      </div>
+    </DefaultLayout>
   );
 }
 
