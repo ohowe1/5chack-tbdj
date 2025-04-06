0a67d3bbe54cecb658e44385765da834249417a9
ohowe <ohowe@duck.com>
2025-04-06 00:46:40 -0700

    OK BOOMER

--- frontend/src/components/posts/PostsTable.tsx
+++ frontend/src/components/posts/PostsTable.tsx
@@ -1,4 +1,3 @@
-import { Key } from "react";
 import { TPostFilled } from "../../../../shared/types/post";
 import PostCard from "./PostCard";
 
@@ -8,7 +7,7 @@ function PostsTable({ posts }: { posts: TPostFilled[] }) {
         {
           posts.length > 0 ? (
             posts.map((post) => (
-                <PostCard post={post} />
+                <PostCard post={post} key={post._id.toString()} />
             ))
           ) : (
             <p className="text-center text-gray-500">No posts available.</p>
