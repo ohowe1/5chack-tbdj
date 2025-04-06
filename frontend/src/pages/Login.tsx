c90aa7e5993eccd548d8f9b1fe56a440b62a32d1
angelinatsai04 <antsai@g.hmc.edu>
2025-04-05 17:22:47 -0700

    login done

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,12 +1,37 @@
 import DefaultLayout from "../components/DefaultLayout";
+import {
+  Anchor,
+  Button,
+  Checkbox,
+  Divider,
+  Group,
+  Paper,
+  PaperProps,
+  PasswordInput,
+  Stack,
+  Text,
+  TextInput,
+  Container,
+} from '@mantine/core';
+import { upperFirst, useToggle } from '@mantine/hooks';
+import { GoogleButton } from '../components/GoogleButton';
 
-function Login() {
+export function Login(props: PaperProps) {
+  const [type, toggle] = useToggle(['login', 'register']);
+
   return (
     <DefaultLayout>
-      <div className="home">
-        <h1>Sign in to bounty with Google</h1>
-        <p>This is the main page of our application.</p>
-      </div>
+      <Container flex={1} size={720} my={50}>
+      <Paper radius="md" p="xl" withBorder {...props}>
+        <Text size="xl" fw={500}>
+          Welcome to bountee, {type} with
+        </Text>
+
+        <Group grow mb="lg" mt="lg" >
+          <GoogleButton size="md" radius="xl">Google</GoogleButton>
+        </Group>
+      </Paper>
+    </Container>
     </DefaultLayout>
   );
 }
